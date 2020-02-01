4dadf20237c7a1006ff29d1980cb061711f4e799
chizzy <cwchiz16@gmail.com>
2020-02-01 10:59:22 -0700

    fixed fs

---
+++ client/build/precache-manifest.9b8c802b6fe0ec19343cb3fe66796196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f793ad65d71b75521a1ca93d701a93",
+    "url": "%7BCollinChiz%7D.github.io/%7Bgooglebooks%7D/index.html"
+  },
+  {
+    "revision": "9374fe07c6febe41475c",
+    "url": "%7BCollinChiz%7D.github.io/%7Bgooglebooks%7D/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "602888f75859786a0491",
+    "url": "%7BCollinChiz%7D.github.io/%7Bgooglebooks%7D/static/js/2.d167201e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "%7BCollinChiz%7D.github.io/%7Bgooglebooks%7D/static/js/2.d167201e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9374fe07c6febe41475c",
+    "url": "%7BCollinChiz%7D.github.io/%7Bgooglebooks%7D/static/js/main.493e97f6.chunk.js"
+  },
+  {
+    "revision": "a8b6564074202f1eed88",
+    "url": "%7BCollinChiz%7D.github.io/%7Bgooglebooks%7D/static/js/runtime-main.514598c7.js"
+  }
+]);
